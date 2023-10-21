8ce9ae68584c61a47f7c604c8d9ba46d00a9590e
Artur <arturmuxutdinov@gmail.com>
2023-10-21 03:10:49 +0300

    fix(hw2): task2
    Added Tinkoff's test; refactored task2

--- src/test/java/edu/hw2/task2/RectangleTest.java
+++ src/test/java/edu/hw2/task2/RectangleTest.java
@@ -2,9 +2,6 @@ package edu.hw2.task2;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RectangleTest {
