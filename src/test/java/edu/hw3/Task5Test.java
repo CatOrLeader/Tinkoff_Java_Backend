924ca80c474ae00ba9e7f38b026e610fc16a5436
Artur <arturmuxutdinov@gmail.com>
2023-10-24 14:09:22 +0300

    feat(hw3): task5
    Added and completed; tested

--- src/test/java/edu/hw3/Task5Test.java
+++ src/test/java/edu/hw3/Task5Test.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 import java.util.InputMismatchException;
 import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.in;
 
 public class Task5Test {
     @Test
