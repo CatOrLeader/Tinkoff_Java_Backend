1f36476ba30e929c414aa4d4d2fb05c4d2321e32
Artur <arturmuxutdinov@gmail.com>
2023-12-22 03:18:05 +0300

    feat(hw10): task2
    Added with tests

---
+++ src/main/java/edu/hw10/Task2.java
@@ -0,0 +1,196 @@
+package edu.hw10;
+
+import java.io.IOException;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+
+public final class Task2 {
+    private Task2() {
+    }
+
+    @Target(ElementType.METHOD)
+    @Retention(RetentionPolicy.RUNTIME)
+    @Inherited
+    @Documented
+    public @interface Cache {
+        boolean persist() default false;
+
+        /**
+         * Provide a folder for the caching of the methods information and return values
+         *
+         * @return string representation (path) of the folder
+         */
+        String onDiskStorage() default "";
+    }
+
+    public record CacheProxy(Object object, Class<?> objectClass) implements InvocationHandler {
+        private static final String INCORRECT_STORAGE_MSG = "Provided storage is not exists OR a directory, not a file";
+
+        private static final String DELIMITER = "_:";
+        private static final String KV_DELIMITER = "-:-";
+        private static final Map<Method, Map<String, String>> RUNTIME_STORAGE = new HashMap<>();
+
+        public static Object create(Object object, Class<?> objectClass) {
+            return Proxy.newProxyInstance(
+                objectClass.getClassLoader(),
+                objectClass.getInterfaces(),
+                new CacheProxy(object, objectClass)
+            );
+        }
+
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            try {
+                String maybeCachedValueString = tryGetFromCache(method, args);
+                if (maybeCachedValueString != null) {
+                    return parseFromStringToObject(maybeCachedValueString, method.getReturnType())[0];
+                }
+            } catch (InvalidPathException ignored) {
+            } catch (IllegalArgumentException exception) {
+                throw new RuntimeException(exception);
+            } catch (Exception ignored) {
+            }
+
+            Object returnedValue = method.invoke(object, args);
+            if (method.isAnnotationPresent(Cache.class)) {
+                cache(method, returnedValue, args);
+            }
+            return returnedValue;
+        }
+
+        private void cache(Method method, Object value, Object[] args) {
+            Cache cache = method.getAnnotation(Cache.class);
+            boolean isPersist = cache.persist();
+
+            if (isPersist) {
+                Path storage = getPathToStorage(cache);
+                if (storage.toFile().isDirectory()) {
+                    throw new IllegalArgumentException(INCORRECT_STORAGE_MSG);
+                }
+                appendToFile(storage, value, args);
+            } else {
+                if (!RUNTIME_STORAGE.containsKey(method)) {
+                    RUNTIME_STORAGE.put(method, new HashMap<>());
+                }
+
+                RUNTIME_STORAGE.get(method).put(
+                    constructArgsString(args), constructValueString(value)
+                );
+            }
+        }
+
+        private static void appendToFile(Path file, Object value, Object[] args) {
+            StringBuilder builder = new StringBuilder(readFromFile(file));
+            builder.append(constructArgsString(args)).append(KV_DELIMITER)
+                .append(constructValueString(value)).append("\n");
+
+            try {
+                Files.createFile(file.toAbsolutePath());
+            } catch (IOException ignored) {
+            }
+
+            try {
+                Files.writeString(file, builder.toString());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        private static String readFromFile(Path file) {
+            if (!file.toFile().exists()) {
+                return "";
+            }
+
+            try {
+                StringBuilder builder = new StringBuilder();
+                Files.readAllLines(file).forEach(line -> builder.append(line).append("\n"));
+                return builder.toString();
+            } catch (IOException e) {
+                throw new IllegalArgumentException("Provided file is incorrect");
+            }
+        }
+
+        private static String tryGetFromCache(Method method, Object[] args) {
+            String stringArgs = constructArgsString(args);
+
+            Cache cache = method.getAnnotation(Cache.class);
+            boolean isPersist = cache.persist();
+
+            if (isPersist) {
+                Path storage = getPathToStorage(cache);
+                if (!storage.toFile().exists() || storage.toFile().isDirectory()) {
+                    throw new InvalidPathException(storage.toString(), INCORRECT_STORAGE_MSG);
+                }
+
+                try {
+                    for (String line : Files.readAllLines(storage)) {
+                        String[] kv = line.split(KV_DELIMITER);
+                        if (kv[0].equals(stringArgs)) {
+                            return kv[1];
+                        }
+                    }
+                    return null;
+                } catch (IOException e) {
+                    throw new RuntimeException("Something went wrong while reading from storage");
+                }
+            } else {
+                if (!RUNTIME_STORAGE.containsKey(method)) {
+                    return null;
+                }
+                var map = RUNTIME_STORAGE.get(method);
+                return map.get(stringArgs);
+            }
+        }
+
+        private static String constructArgsString(Object[] args) {
+            if (args == null) {
+                return "";
+            }
+
+            StringBuilder builder = new StringBuilder();
+            for (Object object : args) {
+                builder.append(object.getClass().cast(object)).append(DELIMITER);
+            }
+            return builder.substring(0, builder.length() - DELIMITER.length());
+        }
+
+        private static String constructValueString(Object value) {
+            return value.getClass().cast(value).toString();
+        }
+
+        private static Object[] parseFromStringToObject(String value, Class<?>... objectTypes) {
+            String[] string = value.split(DELIMITER);
+            Object[] parsedObjects = new Object[string.length];
+            for (int i = 0; i < string.length; i++) {
+                var current = objectTypes[i];
+
+                if (current.equals(Integer.class) || current.equals(int.class)) {
+                    parsedObjects[i] = Integer.parseInt(string[i]);
+                } else if (current.equals(Long.class) || current.equals(long.class)) {
+                    parsedObjects[i] = Long.parseLong(string[i]);
+                } else if (current.equals(String.class)) {
+                    parsedObjects[i] = string[i];
+                } else {
+                    throw new IllegalArgumentException("This type is unsupported: " + current);
+                }
+            }
+            return parsedObjects;
+        }
+
+        private static Path getPathToStorage(Cache cache) {
+            return Path.of(cache.onDiskStorage());
+        }
+    }
+}
---
+++ src/test/java/edu/hw10/Task2Test.java
@@ -0,0 +1,160 @@
+package edu.hw10;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Objects;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static edu.hw10.Task2.Cache;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
+
+public class Task2Test {
+    private static final Path DIRECTORY = Paths.get("src", "test", "resources", "edu", "hw10", "task2");
+    private static final Path STORAGE = DIRECTORY.resolve(Path.of("temp.txt"));
+
+    @AfterEach
+    public void tearUp() throws IOException {
+        for (File file : Objects.requireNonNull(DIRECTORY.toFile().listFiles())) {
+            Files.deleteIfExists(file.toPath());
+        }
+    }
+
+    public interface FibCalculator {
+        @Cache()
+        long fibRuntimeMap(int number);
+
+        @Cache(persist = true, onDiskStorage = "src/test/resources/edu/hw10/task2/temp.txt")
+        long fibFile(int number);
+
+        @Cache(persist = true, onDiskStorage = "src/test/resources/edu/hw10/task2/")
+        int fibIncorrectStorage(int number);
+
+        @Cache()
+        double fibUnsupportedType();
+    }
+
+    public static final class DefaultFibCalculator implements FibCalculator {
+        public DefaultFibCalculator() {
+        }
+
+        @Override
+        public long fibRuntimeMap(int number) {
+            long a = 0;
+            long b = 1;
+            for (int i = 0; i < number; i++) {
+                long temp = a;
+                a = b;
+                b = temp + b;
+            }
+            return a;
+        }
+
+        @Override
+        public long fibFile(int number) {
+            long a = 0;
+            long b = 1;
+            for (int i = 0; i < number; i++) {
+                long temp = a;
+                a = b;
+                b = temp + b;
+            }
+            return a;
+        }
+
+        @Override
+        public int fibIncorrectStorage(int number) {
+            return 0;
+        }
+
+        @Override
+        public double fibUnsupportedType() {
+            return 0;
+        }
+    }
+
+    @Test
+    @DisplayName("Check correctness of fib calculator")
+    void baseCorrectness() {
+        FibCalculator calculator = new DefaultFibCalculator();
+
+        final int[] numbers = new int[] {1, 2, 3, 4, 5};
+        final long[] answers = new long[] {1, 1, 2, 3, 5};
+
+        for (int i = 0; i < numbers.length; i++) {
+            assertThat(calculator.fibRuntimeMap(numbers[i])).isEqualTo(answers[i]);
+        }
+    }
+
+    @Test
+    @DisplayName(
+        "Provide incorrect storage")
+    void incorrectStorage() {
+        final int number = 5;
+        DefaultFibCalculator calculator = new DefaultFibCalculator();
+        FibCalculator proxy = (FibCalculator) Task2.CacheProxy.create(calculator, calculator.getClass());
+
+        assertThatExceptionOfType(RuntimeException.class)
+            .isThrownBy(() -> proxy.fibIncorrectStorage(number));
+    }
+
+    @Test
+    @DisplayName(
+        "Provide unsupported type")
+    void unsupportedType() {
+        DefaultFibCalculator calculator = new DefaultFibCalculator();
+        FibCalculator proxy = (FibCalculator) Task2.CacheProxy.create(calculator, calculator.getClass());
+        proxy.fibUnsupportedType();
+
+        assertThatExceptionOfType(RuntimeException.class)
+            .isThrownBy(proxy::fibUnsupportedType);
+    }
+
+    @Test
+    @DisplayName(
+        "Check if the program works with cache for single value calculation, runtime map contains key and value")
+    void singleValueCachedCalculation_runtimeMap() {
+        final int number = 5;
+        final int answer = 5;
+        DefaultFibCalculator calculator = new DefaultFibCalculator();
+        FibCalculator proxy = (FibCalculator) Task2.CacheProxy.create(calculator, calculator.getClass());
+
+        assertThat(proxy.fibRuntimeMap(number)).isEqualTo(answer);
+        assertThat(proxy.fibRuntimeMap(number)).isEqualTo(answer); // Get value from cache
+    }
+
+    @Test
+    @DisplayName(
+        "Check if the program works with cache for single value calculation, file storage contains key and value")
+    void singleValueCachedCalculation_fileStorage() throws IOException {
+        final int number = 5;
+        final int answer = 5;
+        DefaultFibCalculator calculator = new DefaultFibCalculator();
+        FibCalculator proxy = (FibCalculator) Task2.CacheProxy.create(calculator, calculator.getClass());
+
+        assertThat(proxy.fibFile(number)).isEqualTo(answer);
+        assertThat(proxy.fibFile(number)).isEqualTo(answer); // Get from cache
+        assertThat(STORAGE.toFile().exists()).isTrue();
+        assertThat(Files.readString(STORAGE).trim()).containsIgnoringNewLines("5-:-5");
+    }
+
+    @Test
+    @DisplayName(
+        "Check if the program works with cache for multiple values calculation, file storage contains key and value")
+    void multipleValuesCachedCalculation_fileStorage() throws IOException {
+        final int number = 5;
+        final int answer = 5;
+        DefaultFibCalculator calculator = new DefaultFibCalculator();
+        FibCalculator proxy = (FibCalculator) Task2.CacheProxy.create(calculator, calculator.getClass());
+
+        assertThat(proxy.fibFile(number)).isEqualTo(answer);
+        assertThat(proxy.fibFile(number)).isEqualTo(answer); // Get from cache
+        assertThat(proxy.fibFile(number + 1)).isEqualTo(answer + 3);
+        assertThat(STORAGE.toFile().exists()).isTrue();
+        assertThat(Files.readString(STORAGE).trim()).containsIgnoringNewLines("5-:-5\n6-:-8");
+    }
+}
