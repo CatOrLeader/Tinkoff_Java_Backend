02594efdca84af8c4ec3116ef30d26250bea00ba
Artur <arturmuxutdinov@gmail.com>
2023-12-30 11:11:19 +0300

    fix(hw8): tests
    Adjust tests to satisfy the template

--- src/test/java/edu/hw8/Task1Test.java
+++ src/test/java/edu/hw8/Task1Test.java
@@ -2,6 +2,7 @@ package edu.hw8;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
@@ -44,15 +45,17 @@ public class Task1Test {
     @Test
     @DisplayName("Single thread, all type of messages")
     void singleThread_CorrectMessages() throws IOException {
-        String[] messages = new String[] {"личности", "оскорбления", "глупый", "интеллект", "incorrect"};
+        final String[] messages = new String[] {"личности", "оскорбления", "глупый", "интеллект", "incorrect"};
         Client client = new Client(LOCALHOST, PORT);
 
-        List<String> actualAnswers = new ArrayList<>();
-        for (String message : messages) {
-            actualAnswers.add(client.sendAndGet(message));
-        }
-
-        List<String> expectedAnswers = List.of(
+        var actualAnswers = Arrays.stream(messages).map(string -> {
+            try {
+                return client.sendAndGet(string);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }).toList();
+        var expectedAnswers = List.of(
             "Не переходи на личности там, где их нет",
             "Если твои противники перешли на личные оскорбления, будь уверена — твоя победа не за горами",
             "А я тебе говорил, что ты глупый? Так вот, я забираю свои слова обратно... Ты просто бог идиотизма.",
@@ -67,27 +70,31 @@ public class Task1Test {
     @DisplayName("More than threshold threads, all type of messages")
     void moreThanThresholdThreads_CorrectMessages() {
         int currentThreadsNumber = 100;
-        String message = "личности";
-        List<String> actualAnswers = Collections.synchronizedList(new ArrayList<>());
-        try (ExecutorService service = Executors.newFixedThreadPool(currentThreadsNumber)) {
-            for (int i = 0; i < currentThreadsNumber; i++) {
-                service.execute(() -> {
-                    try {
-                        Client client = new Client(LOCALHOST, PORT);
-                        actualAnswers.add(client.sendAndGet(message));
-                        client.disconnect();
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
-                });
-            }
-        }
 
-        List<String> expectedAnswers = new ArrayList<>();
-        for (int i = 0; i < currentThreadsNumber; i++) {
-            expectedAnswers.add("Не переходи на личности там, где их нет");
-        }
+        var actualAnswers = sendNMessagesSimultaneouslyAndGetListOfAnswers(currentThreadsNumber, "личности");
+        var expectedAnswers =
+            Collections.nCopies(currentThreadsNumber, "Не переходи на личности там, где их нет");
 
         assertThat(actualAnswers).containsExactlyInAnyOrderElementsOf(expectedAnswers);
+    }
+
+    private static List<String> sendNMessagesSimultaneouslyAndGetListOfAnswers(int N, String message) {
+        List<String> actualAnswers = Collections.synchronizedList(new ArrayList<>());
+        try (ExecutorService service = Executors.newFixedThreadPool(N)) {
+            for (int i = 0; i < N; i++) {
+                service.execute(() -> sendMessageAndPushAnswerToTheList(actualAnswers, message));
+            }
+        }
+        return actualAnswers;
+    }
+
+    private static void sendMessageAndPushAnswerToTheList(List<String> list, String message) {
+        try {
+            Client client = new Client(LOCALHOST, PORT);
+            list.add(client.sendAndGet(message));
+            client.disconnect();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/test/java/edu/hw8/Task2Test.java
+++ src/test/java/edu/hw8/Task2Test.java
@@ -5,90 +5,54 @@ import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.RejectedExecutionException;
 import java.util.concurrent.atomic.AtomicInteger;
+import org.apache.logging.log4j.LogManager;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static edu.hw8.Task2.ThreadPool;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
-import static org.assertj.core.api.Assertions.fail;
 
 public class Task2Test {
     @Test
     @DisplayName("Incorrect pool size")
     void incorrectPoolSize() {
-        int size = -1;
-
         assertThatExceptionOfType(IllegalArgumentException.class)
-            .isThrownBy(() -> {
-                try (ThreadPool ignored = Task2.FixedThreadPool.create(size)) {
-                    fail("");
-                }
-            });
+            .isThrownBy(() -> tryToCreateThreadPoolWithIncorrectSize(-1));
     }
 
     @Test
     @DisplayName("Null task")
     void nullTask() {
         assertThatExceptionOfType(NullPointerException.class)
-            .isThrownBy(() -> {
-                try (ThreadPool pool = Task2.FixedThreadPool.create(1)) {
-                    pool.execute(null);
-                }
-            });
+            .isThrownBy(() -> tryToCreateThreadPoolWithNullTask(1));
     }
 
     @Test
     @DisplayName("Insertion of the task after termination")
     void insertionAfterTermination() {
-        Runnable runnable = () -> System.out.println("I'm stupid task");
-
         assertThatExceptionOfType(RejectedExecutionException.class)
             .isThrownBy(() -> {
-                ThreadPool pool = Task2.FixedThreadPool.create(1);
-                pool.execute(runnable);
-                pool.start();
-                pool.close();
-                pool.execute(runnable);
+                tryToRunAfterTermination(1, () -> System.out.println("I'm stupid task"));
             });
     }
 
     @Test
     @DisplayName("Simple pool testing")
-    void simplePoolTesting() throws Exception {
+    void simplePoolTesting() {
         final int threadCount = 10;
-        List<String> actualValue = Collections.synchronizedList(new ArrayList<>());
 
-        try (ThreadPool pool = Task2.FixedThreadPool.create(5)) {
-            for (int i = 0; i < threadCount; i++) {
-                pool.execute(() -> actualValue.add("bob"));
-            }
-            pool.start();
-        }
-        List<String> expectedValues = new ArrayList<>();
-        for (int i = 0; i < threadCount; i++) {
-            expectedValues.add("bob");
-        }
+        var actualValues = executeSimpleTaskNTimesAndGetListOfResults(threadCount);
+        List<String> expectedValues = Collections.nCopies(threadCount, "bob");
 
-        assertThat(actualValue).containsExactlyInAnyOrderElementsOf(expectedValues);
+        assertThat(actualValues).containsExactlyInAnyOrderElementsOf(expectedValues);
     }
 
     @Test
     @DisplayName("Multithreading fibonacci testing")
     void multithreadingFibonacciTesting() throws Exception {
         final int threadsCount = 30;
-        AtomicInteger current = new AtomicInteger(1);
-        List<Integer> actualValues = Collections.synchronizedList(new ArrayList<>());
-        try (ThreadPool pool = Task2.FixedThreadPool.create(5)) {
-            for (int i = 0; i < threadsCount; i++) {
-                pool.execute(() -> {
-                    int answer = fibonacci(current.incrementAndGet());
-                    actualValues.add(answer);
-                });
-            }
-            pool.start();
-        }
-        Thread.sleep(100);
 
+        List<Integer> actualValues = calculateNFibonacciNumbersUsingThreadPool(threadsCount);
         List<Integer> expectedValues = List.of(
             1,
             2,
@@ -131,5 +95,69 @@ public class Task2Test {
         }
 
         return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    private static void tryToCreateThreadPoolWithIncorrectSize(int size) {
+        try (ThreadPool ignored = Task2.FixedThreadPool.create(size)) {
+            LogManager.getLogger().info("No exception was thrown by incorrect size");
+        } catch (IllegalArgumentException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void tryToCreateThreadPoolWithNullTask(int size) {
+        try (ThreadPool pool = Task2.FixedThreadPool.create(size)) {
+            pool.execute(null);
+        } catch (NullPointerException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void tryToRunAfterTermination(int size, Runnable runnable) {
+        try (ThreadPool pool = Task2.FixedThreadPool.create(size)) {
+            pool.execute(runnable);
+            pool.start();
+            pool.close();
+            pool.execute(runnable);
+        } catch (RejectedExecutionException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static List<String> executeSimpleTaskNTimesAndGetListOfResults(int N) {
+        List<String> list = Collections.synchronizedList(new ArrayList<>());
+        try (ThreadPool pool = Task2.FixedThreadPool.create(N)) {
+            for (int i = 0; i < N; i++) {
+                pool.execute(() -> list.add("bob"));
+            }
+            pool.start();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return list;
+    }
+
+    private static List<Integer> calculateNFibonacciNumbersUsingThreadPool(int N) throws InterruptedException {
+        AtomicInteger current = new AtomicInteger(1);
+        List<Integer> list = Collections.synchronizedList(new ArrayList<>());
+        try (ThreadPool pool = Task2.FixedThreadPool.create(N)) {
+            for (int i = 0; i < N; i++) {
+                pool.execute(() -> {
+                    int answer = fibonacci(current.incrementAndGet());
+                    list.add(answer);
+                });
+            }
+            pool.start();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        Thread.sleep(100);
+        return list;
     }
 }
